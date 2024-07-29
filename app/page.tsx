f0041bb2d954ceb3bac02b19addc7b1137b0ef9b
basuabhirup <basu.abhirup27@gmail.com>
2024-07-29 18:48:06 +0530

    Implement basic UI of pantry items

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,54 @@
-import { Box } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
+
+const items = [
+  "tomato",
+  "potato",
+  "onion",
+  "garlic",
+  "ginger",
+  "carrot",
+  "lettuce",
+];
 
 export default function Home() {
   return (
-    <Box width="100vw" height="100vh" display={"flex"} justifyContent={"center"} alignItems={"center"}>
-      Hello World
+    <Box
+      width="100vw"
+      height="100vh"
+      display={"flex"}
+      justifyContent={"center"}
+      alignItems={"center"}
+    >
+      <Box>
+        <Box
+          width="800px"
+          height="100px"
+          bgcolor="#ADD8E6"
+          display={"flex"}
+          justifyContent={"center"}
+          alignItems={"center"}
+          marginBottom={"2px"}
+        >
+          <Typography variant="h3" color="#333" textAlign="center">
+            Pantry Items
+          </Typography>
+        </Box>
+        <Stack width="800px" height="600px" spacing={2} overflow="auto">
+          {items.map((item) => (
+            <Box
+              key={item}
+              width="100%"
+              minHeight="100px"
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+              bgcolor="#e0f0ff"
+            >
+              {item.charAt(0).toUpperCase() + item.slice(1)}
+            </Box>
+          ))}
+        </Stack>
+      </Box>
     </Box>
   );
 }
